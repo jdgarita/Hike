42d5645e488b223f1d1e3d9ff18c8241fb970326
JD <juandiegogarita@gmail.com>
2023-07-25 22:04:18 -0600

    adds settings sheet

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -11,6 +11,7 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     func randomImage() {
         repeat {
@@ -46,9 +47,14 @@ struct CardView: View {
                         Spacer()
                         
                         Button{
-                    
+                            isShowingSheet.toggle()
                         } label: {
                             CustomButtonView()
+                        }
+                        .sheet(isPresented: $isShowingSheet){
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     
